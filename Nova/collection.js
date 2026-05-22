97dad650eafd90d1a03a180885c912d5a6e6e0c0
KISHORE-NS <kishore.n.s.0201@gmail.com>
2026-05-22 20:06:42 +0530

    Add files via upload

---
+++ Nova/collection.js
@@ -0,0 +1,33 @@
+var productcontainer=document.getElementById("product")
+var search=document.getElementById("search")
+var productlist = productcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(event){
+    var enteredvalue =event.target.value.toUpperCase()
+
+    for (count=0;count<productlist.length;count++)
+    {
+        var productname = productlist[count].querySelector("p").textContent
+
+        if(productname.toUpperCase().indexOf(enteredvalue)<0)
+        {
+            productlist[count].style.display ="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+})
+
+
+
+var sidenav=document.querySelector(".side-nav")
+
+
+function shownav(){
+    sidenav.style.left = "0%"
+}
+
+function closenav(){
+    sidenav.style.left="-60%"
+}
